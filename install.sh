bd8b5ba6c4dbe18046e699632c6734647a0e55f9
Michael Schade <michael@spearheaddev.com>
2013-01-11 22:55:10 -0800

    switch to oh my zsh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+git submodule update --recursive
+
 ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/vim/vimrc $HOME/.vimrc
 
+cp -R $PWD/zsh-custom/* $PWD/zsh
 ln -s $PWD/zsh $HOME/.zsh
 ln -s $PWD/zsh/zshrc $HOME/.zshrc
--- zsh-custom/custom/michael.zsh
+++ zsh-custom/custom/michael.zsh
@@ -18,12 +18,22 @@ autoload -U compinit && compinit
 
 # basic environment variables
 export EDITOR='vim'
-export PATH="$PATH:/usr/local/bin:$HOME/.bin:$HOME/Library/Haskell/bin"
+export PATH="$PATH \
+           :/usr/local/sbin \
+           :$HOME/.bin \
+           :$HOME/Library/Haskell/bin \
+           :$HOME/node_modules/.bin \
+           "
 export TERM='xterm-256color'
 
 # some nice little alias tweaks
 alias ls='ls -G'
 
-source ~/.zsh/keybindings   # makes the delete and home keys work
-source ~/.zsh/prompt        # pretty prompt configuration
-source ~/.zsh/path          # path configuraton
+export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+
+export PATH=$PATH:$HOME/.usr/bin
+export PYTHONPATH=/Library/Frameworks/Python.framework/Versions/2.5/lib/python2.5/site-packages/
+export EDITOR=/usr/bin/vim
+
+# No spaces (easy pretty printing, and I don't use spaces in dir names anyway)
+export PATH=`echo $PATH | sed -E 's/ *//g'`
