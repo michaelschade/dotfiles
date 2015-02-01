5821b2412b6a2e912b18fa730fcac6812a8206d7
Michael Schade <m@mschade.me>
2015-02-01 01:37:40 -0500

    add tmux

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 git submodule update --init --recursive
 
+ln -s $PWD/tmux $HOME/.tmux
+ln -s $PWD/tmux/tmux.conf $HOME/.tmux.conf
+
 ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/vim/vimrc $HOME/.vimrc
 
