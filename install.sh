bbd1749d3a22345ddc17368122e9144557ba1939
Michael Schade <michael@stripe.com>
2015-05-26 18:28:20 -0700

    handle existing symbolically linked directories better

--- install.sh
+++ install.sh
@@ -2,19 +2,19 @@
 
 git submodule update --init --recursive
 
-ln -fs $PWD/tmux $HOME/.tmux
+ln -fs $PWD/tmux $HOME/.tmux/
 ln -fs $PWD/tmux/tmux.conf $HOME/.tmux.conf
 
 # setup vim
 brew install ctags-exuberant the_silver_searcher
 $PWD/vim/bundle/YouCompleteMe/install.sh --clang-completer
-ln -fs $PWD/vim $HOME/.vim
+ln -fs $PWD/vim $HOME/.vim/
 ln -fs $PWD/vim/vimrc $HOME/.vimrc
 mkdir -p $HOME/.vimswap
 
 # setup zsh
 cp -R $PWD/zsh-custom/* $PWD/zsh
-ln -fs $PWD/zsh $HOME/.zsh
+ln -fs $PWD/zsh $HOME/.zsh/
 ln -fs $PWD/zsh/zshrc $HOME/.zshrc
 
 ln -fs $PWD/Xresources $HOME/.Xresources
