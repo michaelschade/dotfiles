13b6bab6a7bc1f2e61aa9bab9ade38b0e569d918
Michael Schade <github@mschade.me>
2017-12-29 22:01:26 +0000

    add better support for raspberry pi

--- install.sh
+++ install.sh
@@ -6,8 +6,16 @@ ln -nsf $PWD/tmux $HOME/.tmux
 ln -nsf $PWD/tmux/tmux.conf $HOME/.tmux.conf
 
 # setup vim
-brew install cmake ctags-exuberant the_silver_searcher
-$PWD/vim/bundle/YouCompleteMe/install.sh --clang-completer
+if [ `uname` = "Darwin" ]; then
+  brew install cmake ctags-exuberant the_silver_searcher
+else
+  sudo apt-get install vim cmake exuberant-ctags silversearcher-ag
+fi
+if [ $WITHOUT_YCM != 1 ]; then
+  $PWD/vim/bundle/YouCompleteMe/install.sh --clang-completer --system-libclang
+else
+  echo "Skipping YCM"
+fi
 ln -nsf $PWD/vim $HOME/.vim
 ln -nsf $PWD/vim/vimrc $HOME/.vimrc
 mkdir -p $HOME/.vimswap
