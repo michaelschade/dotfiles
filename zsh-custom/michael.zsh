e3826d618f803aeb02e9c78ded6de0bd45f3d79b
Michael Schade <github@mschade.me>
2020-01-23 19:46:12 -0800

    update configs

--- zsh-custom/michael.zsh
+++ zsh-custom/michael.zsh
@@ -17,17 +17,27 @@ bindkey -e
 autoload -U compinit && compinit
 
 # basic environment variables
+export PGUSER=postgres
+export PGPASSWORD=postgres
 export EDITOR='vim'
 export GOPATH=$HOME/Dropbox/code/go
 export MYGO=$GOPATH/src/github.com/michaelschade
 export PATH="$PATH \
            :$HOME/node_modules/.bin \
            :$HOME/.rvm/bin \
+           :$HOME/.cargo/bin \
            :/usr/local/lib/python3.5/site-packages \
-           :$GOPATH/bin
+           :$GOPATH/bin \
+           :/usr/local/opt/libpq/bin \
            "
 export PATH=`echo $PATH | sed -E 's/ *//g'`
+source $HOME/.ghcup/env
 [[ $TMUX = "" ]] && export TERM="xterm-256color"
+
+# nvm
+# export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # some nice little alias tweaks
 alias ls='ls -G'
@@ -40,4 +50,9 @@ if [ `uname` = "Darwin" ]; then
   alias vim='mvim -v'
 fi
 
-alias excelsior="ssh excelsior -t tmux a"
+# electron
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/usr/local/opt/libffi/lib/pkgconfig"
+
+# other
+alias excelsior="ssh michael@excelsior"
+
