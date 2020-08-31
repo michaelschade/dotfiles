7fab4f55467525addf3f8fff9aaab237b2123036
Michael Schade <michael@kenchi.com>
2020-08-31 16:37:40 -0700

    install tmux node libpq

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ ln -nsf $PWD/tmux/tmux.conf $HOME/.tmux.conf
 
 # setup vim
 if [ `uname` = "Darwin" ]; then
-  brew install cmake ctags-exuberant the_silver_searcher neovim
+  brew install cmake ctags-exuberant the_silver_searcher neovim tmux node libpq
 else
   sudo apt-get install vim cmake exuberant-ctags silversearcher-ag
 fi
