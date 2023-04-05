7adfcdaab32090ca340133c039a640aa8e5326d8
Michael Schade <github@mschade.me>
2023-04-05 15:07:17 -0400

    refactor install.sh

--- install.sh
+++ install.sh
@@ -1,45 +1,69 @@
 #!/bin/sh
 
-if ! [ -x "$(command -v brew)" ]; then
+check_dependency() {
+  if ! [ -x "$(command -v $1)" ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+install_brew() {
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
+}
 
-if ! [ -x "$(command -v git)" ]; then
-  brew install git
-fi
-
-ln -nsf $PWD/gitconfig $HOME/.gitconfig
-git submodule update --recursive --remote
-
-ln -nsf $PWD/tmux $HOME/.tmux
-ln -nsf $PWD/tmux/tmux.conf $HOME/.tmux.conf
-
-# setup vim
-if [ `uname` = "Darwin" ]; then
+install_packages_darwin() {
   brew install cmake ctags-exuberant the_silver_searcher neovim tmux node libpq jq
-else
-  sudo apt-get install vim cmake exuberant-ctags silversearcher-ag
-fi
-
-if [ `uname` = "Darin" ]; then
   brew install pyenv pyenv-virtualenv
+}
+
+install_packages_linux() {
+  sudo apt-get install vim cmake exuberant-ctags silversearcher-ag
+}
+
+create_symlinks() {
+  ln -nsf $PWD/gitconfig $HOME/.gitconfig
+  ln -nsf $PWD/tmux $HOME/.tmux
+  ln -nsf $PWD/tmux/tmux.conf $HOME/.tmux.conf
+  ln -nsf $PWD/vim $HOME/.vim
+  ln -nsf $PWD/vim/vimrc $HOME/.vimrc
+  ln -nsf $PWD/vim $HOME/.config/nvim
+  ln -nsf $PWD/nvim/init.vim $HOME/.config/nvim/init.vim
+  ln -nsf $PWD/zsh-custom $HOME/.zsh-custom
+  ln -nsf $PWD/zsh-custom/zshrc $HOME/.zshrc
+  ln -nsf $PWD/zsh $HOME/.zsh
+  ln -nsf $PWD/Xresources $HOME/.Xresources
+}
+
+setup_vimproc() {
+  pushd $PWD/vim/bundle/vimproc
+  make
+  popd
+}
+
+create_directories() {
+  mkdir -p $HOME/.vimswap
+  mkdir -p $HOME/.config
+}
+
+# Ensure dependencies are installed
+check_dependency brew || install_brew
+check_dependency git || brew install git
+
+# Install packages based on OS
+if [ "$(uname)" = "Darwin" ]; then
+  install_packages_darwin
+else
+  install_packages_linux
 fi
 
-# vimproc for typescript
-pushd $PWD/vim/bundle/vimproc
-make
-popd
+# Create required directories
+create_directories
 
-ln -nsf $PWD/vim $HOME/.vim
-ln -nsf $PWD/vim/vimrc $HOME/.vimrc
-mkdir -p $HOME/.vimswap
-mkdir -p $HOME/.config
-ln -nsf $PWD/vim $HOME/.config/nvim
-ln -nsf $PWD/nvim/init.vim $HOME/.config/nvim/init.vim
+# Create symbolic links for configuration files
+create_symlinks
 
-# setup zsh
-ln -nsf $PWD/zsh-custom $HOME/.zsh-custom
-ln -nsf $PWD/zsh-custom/zshrc $HOME/.zshrc
-ln -nsf $PWD/zsh $HOME/.zsh
+# Setup vimproc for typescript
+setup_vimproc
 
-ln -nsf $PWD/Xresources $HOME/.Xresources
+git submodule update --recursive --remote
